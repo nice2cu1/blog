81f3a6cecd18c34738dc934778add0fd8e1ddd09
nice2cu1 <nice2cu1@outlook.com>
2023-02-01 15:39:41 +0800

    auto theme

--- themes/github-style/static/js/theme-mode.js
+++ themes/github-style/static/js/theme-mode.js
@@ -18,6 +18,38 @@ function switchTheme() {
   }
 }
 
+
+function themeLister(){
+  let media = window.matchMedia('(prefers-color-scheme: dark)');
+  let callback = (e) => {
+    autoSwitchTheme();
+  };
+  if (typeof media.addEventListener === 'function') {
+    media.addEventListener('change', callback);
+  } else if (typeof media.addListener === 'function') {
+    media.addListener(callback);
+  }
+}
+
+function autoSwitchTheme() {
+  const currentStyle = currentTheme();
+  const iconElement = document.getElementById('github-icon');
+  const systemDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  if (!systemDarkMode && currentStyle === 'dark') {
+    setTheme('light');
+    if (iconElement) {
+      iconElement.removeAttribute('color');
+      iconElement.removeAttribute('class');
+    }
+  } else if (systemDarkMode && currentStyle === 'light') {
+    setTheme('dark');
+    if (iconElement) {
+      iconElement.setAttribute('class', 'octicon');
+      iconElement.setAttribute('color', '#f0f6fc');
+    }
+  }
+}
+
 function setTheme(style) {
   document.querySelectorAll('.isInitialToggle').forEach(elem => {
     elem.classList.remove('isInitialToggle');
